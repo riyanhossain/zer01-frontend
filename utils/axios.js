bdab7c250adb9ab3dc783e6c4b96f4d6d19da5e7
mehrabriyan <mehraabriyan@gmail.com>
2023-03-16 13:09:03 +0600

    url updated

--- utils/axios.js
+++ utils/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 // http://localhost:5000
+//https://jellyfish-app-br4i7.ondigitalocean.app/
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "//https://jellyfish-app-br4i7.ondigitalocean.app",
 });
 
 export default axiosInstance;
